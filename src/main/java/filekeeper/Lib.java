23cc7bbc7af93acfb04c248e621e1101b900dfec
Angor <angor@inbox.ru>
2016-05-17 10:37:04 +0300

    Поправил структуру проекта согласно Git рекомендациям

--- src/main/java/filekeeper/Lib.java
+++ src/main/java/filekeeper/Lib.java
@@ -24,109 +24,106 @@ import java.util.ArrayList;
 
 public class Lib {
 
-	private static Logger	log	= LoggerFactory.getLogger(Lib.class);
+    private static Logger log = LoggerFactory.getLogger(Lib.class);
 
-	public static ArrayList<Task> readXml(Path file) {
-		ArrayList<Task> taskList = new ArrayList<Task>();
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder;
+    public static ArrayList<Task> readXml(Path file) {
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
 
-		try {
-			builder = factory.newDocumentBuilder();
-			Document document = builder.parse(file.toFile());
+        try {
+            builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file.toFile());
 
-			NodeList tasks = document.getElementsByTagName("task");
-			for (int i = 0; i < tasks.getLength(); i++) {
-				Task task = new Task();
-				Node node = tasks.item(i);
-				NamedNodeMap map = node.getAttributes();
-				task.setName(map.getNamedItem("name").getTextContent());
-				task.setSource(Paths.get(map.getNamedItem("source").getTextContent()));
-				task.setDest(Paths.get(map.getNamedItem("dest").getTextContent()));
-				if (map.getNamedItem("mode").getTextContent().equals("true")) {
-					task.setFastMode(true);
-				} else {
-					task.setFastMode(false);
-				}
-				taskList.add(task);
-			}
-		} catch (ParserConfigurationException e) {
-			log.error(e.getLocalizedMessage());
-		} catch (SAXException e) {
-			log.error(e.getLocalizedMessage());
-		} catch (IOException e) {
-			log.error(e.getLocalizedMessage());
-		}
+            NodeList tasks = document.getElementsByTagName("task");
+            for (int i = 0; i < tasks.getLength(); i++) {
+                Task task = new Task();
+                Node node = tasks.item(i);
+                NamedNodeMap map = node.getAttributes();
+                task.setName(map.getNamedItem("name").getTextContent());
+                task.setSource(Paths.get(map.getNamedItem("source").getTextContent()));
+                task.setDest(Paths.get(map.getNamedItem("dest").getTextContent()));
+                if (map.getNamedItem("mode").getTextContent().equals("true")) {
+                    task.setFastMode(true);
+                } else {
+                    task.setFastMode(false);
+                }
+                taskList.add(task);
+            }
+        } catch (ParserConfigurationException e) {
+            log.error(e.getLocalizedMessage());
+        } catch (SAXException e) {
+            log.error(e.getLocalizedMessage());
+        } catch (IOException e) {
+            log.error(e.getLocalizedMessage());
+        }
 
-		return taskList;
+        return taskList;
 
-	}
+    }
 
-	/**
-	 * Сравнение двух файлов двумя вариантами, медленным но очень надёжным и быстрым но с низкой надёжностью.
-	 * @param source путь к файлу образцу
-	 * @param dest путь к проверяемому файлу
-	 * @param fastMode <b>true</b> быстрый режим с низкой надёжностью
-	 * @return
-	 */
-	public static boolean isSameFile(Path source, Path dest, boolean fastMode) {
+    /**
+     * Сравнение двух файлов двумя вариантами, медленным но очень надёжным и быстрым но с низкой надёжностью.
+     *
+     * @param source   путь к файлу образцу
+     * @param dest     путь к проверяемому файлу
+     * @param fastMode <b>true</b> быстрый режим с низкой надёжностью
+     * @return
+     */
+    public static boolean isSameFile(Path source, Path dest, boolean fastMode) {
 
-		// проверка на наличие файла назначения
-		if (Files.notExists(dest)) {
-			return false;
-		}
+        // проверка на наличие файла назначения
+        if (Files.notExists(dest)) {
+            return false;
+        }
 
-		// проверка на размер
-		if (source.toFile().length() != dest.toFile().length()) {
-			return false;
-		}
+        // проверка на размер
+        if (source.toFile().length() != dest.toFile().length()) {
+            return false;
+        }
 
-		// TODO проверка на атрибуты очень сомнительная надёжность но высокая
-		// скорость
-		if (fastMode) {
-			try {
-				if (Files.getLastModifiedTime(source).equals(Files.getLastModifiedTime(dest))) {
-					return true;
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+        if (fastMode) {
+            try {
+                if (Files.getLastModifiedTime(source).equals(Files.getLastModifiedTime(dest))) {
+                    return true;
+                }
+            } catch (IOException e) {
+                log.debug(e.getMessage());
+                return false;
+            }
+        } else {
+            String sourceSHA = doHash(source);
+            String destSHA = doHash(dest);
+            if (sourceSHA.equals(destSHA)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-		// сравнение хешей всего файла
-		// добавить более интелектуальный алгоритм хэша
-		String sourceSHA = doHash(source);
-		String destSHA = doHash(dest);
-		if (sourceSHA.equals(destSHA)) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Подсчёт хэш суммы по всему файлу по алгоритму SHA-256
+     *
+     * @param fileName
+     * @return
+     */
+    private static String doHash(Path fileName) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            log.debug(e.getMessage());
+        }
+        try (InputStream is = Files.newInputStream(fileName)) {
+            DigestInputStream dis = new DigestInputStream(is, md);
 
-	/**
-	 * Подсчёт хэш суммы по всему файлу по алгоритму SHA-256
-	 * @param fileName
-	 * @return
-	 */
-	private static String doHash(Path fileName) {
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		try (InputStream is = Files.newInputStream(fileName)) {
-			DigestInputStream dis = new DigestInputStream(is, md);
-
-			byte[] buffer = new byte[4096];
-			while ((dis.read(buffer)) != -1) {
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		byte[] digest = md.digest();
-		return new BigInteger(1, digest).toString(16);
-	}
+            byte[] buffer = new byte[4096];
+            while ((dis.read(buffer)) != -1) {
+            }
+        } catch (IOException e) {
+            log.debug(e.getMessage());
+        }
+        byte[] digest = md.digest();
+        return new BigInteger(1, digest).toString(16);
+    }
 }
