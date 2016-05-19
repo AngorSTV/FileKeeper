0c1cf0c6a69205512986af56da4c900150ce1663
Angor <angor@inbox.ru>
2016-05-19 10:53:01 +0300

    Первый удачный запуск.

--- src/main/java/FX/Main.java
+++ src/main/java/FX/Main.java
@@ -4,12 +4,20 @@ package FX;/**
 
 import filekeeper.FileKeeper;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 public class Main extends Application {
     private static Logger log = LoggerFactory.getLogger(Main.class);
+    //private Stage primaryStage;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,5 +26,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         log.info("Start JavaFX8 version." + FileKeeper.version);
+        //this.primaryStage = primaryStage;
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getClassLoader().getResource("mainFrm.fxml"));
+            Node node = loader.load();
+            MainFrmCtrl controller = loader.getController();
+            controller.setMainClass(this);
+            primaryStage.setScene(new Scene((Parent)node));
+            primaryStage.setTitle("File Keeper v" + FileKeeper.version);
+            primaryStage.getIcons().addAll(new Image("images/file-keeper-32-icon.png"));
+            primaryStage.show();
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+        }
+
     }
 }
--- src/main/java/FX/MainFrmCtrl.java
+++ src/main/java/FX/MainFrmCtrl.java
@@ -1,7 +1,20 @@
 package FX;
 
+import javafx.fxml.FXML;
+
 /**
  * Created by Андрей on 19.05.2016.
  */
-public class MainFrmCtrl {
+public class MainFrmCtrl{
+
+    Main mainClass;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    public void setMainClass(Main mainClass) {
+        this.mainClass = mainClass;
+    }
 }
